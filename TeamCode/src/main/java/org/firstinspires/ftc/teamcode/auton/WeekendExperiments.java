f717477c64c0b85c4f7e3bed5d0eb6610a4bd3bb
deanv <ftc.stanislastechacademy@gmail.com>
2023-01-03 18:04:12 +0100

    Hi please commit.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/WeekendExperiments.java
@@ -0,0 +1,319 @@
+/*
+ * Copyright (c) 2021 OpenFTC Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.DrivingMethods;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+@Autonomous(name = "Autonomous Dean Weekend", group = "Concept")
+public class WeekendExperiments extends LinearOpMode
+{
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+ // Tag ID 18 from the 36h11 family
+    int LEFT = 1;
+    int MIDDLE = 2;
+    int RIGHT = 3;
+
+    AprilTagDetection tagOfInterest = null;
+    private DcMotor leftBack;
+    private DcMotor leftFront;
+    private DcMotor rightBack;
+    private DcMotor rightFront;
+    private DcMotor armMotor;
+    private Servo intakeServo;
+    int currentHeight = 0;
+
+    public void initAutonomous() {
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        armMotor = hardwareMap.get(DcMotor.class, "armmotor1");
+        intakeServo = hardwareMap.get(Servo.class, "Intake");
+
+        leftBack.setDirection(Direction.FORWARD);
+        rightBack.setDirection(Direction.FORWARD);
+        leftFront.setDirection(Direction.FORWARD);
+        rightFront.setDirection(Direction.FORWARD);
+        armMotor.setDirection(Direction.REVERSE);
+    }
+    public void drive(int leftBackTicks, int rightBackTicks, int leftFrontTicks, int rightFrontTicks) {
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+
+        leftBack.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        rightBack.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        leftFront.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        rightFront.setMode(DcMotor.RunMode.RESET_ENCODERS);
+
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+
+        leftBack.setTargetPosition(leftBackTicks);
+        rightBack.setTargetPosition(rightBackTicks);
+        leftFront.setTargetPosition(leftFrontTicks);
+        rightFront.setTargetPosition(rightFrontTicks);
+
+        leftBack.setPower(0.5);
+        rightBack.setPower(0.5);
+        leftFront.setPower(0.5);
+        rightFront.setPower(0.5);
+
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while(leftBack.isBusy() && rightBack.isBusy() && leftFront.isBusy() && rightFront.isBusy()) {
+            idle();
+        }
+    }
+    public void driveLeft(int Ticks) {
+        drive(Ticks, Ticks, -Ticks, -Ticks);
+    }
+    public void driveRight(int Ticks) {
+        drive(-Ticks, -Ticks, Ticks, Ticks);
+    }
+    public void driveForward(int Ticks) {
+        drive(Ticks, -Ticks, Ticks, -Ticks);
+    }
+    public void driveBackward(int Ticks) {
+        drive(-Ticks, Ticks, -Ticks, Ticks);
+    }
+    public void turnLeft (int degrees) {int Ticks = degrees * 70 / 6; drive(-Ticks, -Ticks, -Ticks, -Ticks); }
+    public void turnRight (int degrees) {int Ticks = degrees * 70 / 6; drive (Ticks, Ticks, Ticks, Ticks); }
+    public void armMovement(int armTicks) {
+        armMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        armMotor.setTargetPosition(armTicks);
+        armMotor.setPower(0.5);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (armMotor.isBusy()) {
+            idle();
+        }
+    }
+
+    /** input ibtergers beschrijving. Output: real
+     * beschrijf kort en krachtig de werking van de functoe
+     * @param servoPosition
+     */
+    public void servoPositioning(double servoPosition) {intakeServo.setPosition(servoPosition);} /*pos 0.3 = closed, pos 0.5 = open*/
+    public int armGet (int stackNum) {
+        int[][] stack = { {0, 400}, {400, 350}, {420, 320}, {525, 300}, {625, 280} };
+        servoPositioning(0.5);
+        armMovement(stack[stackNum][0] - currentHeight);
+        driveForward(stack[stackNum][1]);
+        servoPositioning(0.3);
+        armMovement(400);
+        driveBackward(stack[stackNum][1]);
+        return armMotor.getCurrentPosition();
+    }
+    public int armDrop (int junctionNum) {
+        int[] junction = {150, 1100, 2100, 3000};
+        armMovement(junction[junctionNum] - currentHeight);
+        driveForward(350);
+        armMovement(-400);
+        servoPositioning(0.5);
+        driveBackward(380);
+        return armMotor.getCurrentPosition();
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            }
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+        initAutonomous();
+        waitForStart();
+        servoPositioning(0.3);
+        driveForward(2400);
+        turnLeft(90);
+        armDrop(3);
+
+//        initAutonomous();
+//        waitForStart();
+//        servoPositioning(0.3);
+//        driveForward(1600);
+//        driveLeft(750);
+//        armMovement(2100);
+//        driveForward(350);
+//        armMovement(-2100);
+//        servoPositioning(0.5);
+//        driveBackward(380);
+//        driveRight(2400);
+//        driveForward(600);
+//        turnRight(37);
+
+       if(tagOfInterest == null || tagOfInterest.id == LEFT){
+           telemetry.addLine("Going for left");
+       }else if(tagOfInterest.id == MIDDLE){
+           telemetry.addLine("Going for centre");
+       }else if(tagOfInterest.id == RIGHT){
+           telemetry.addLine("Going for right");
+       }
+    }
+
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+}
